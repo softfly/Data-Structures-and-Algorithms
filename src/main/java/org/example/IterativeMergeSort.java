5c4a256f9bb1cd401a93a76318949599b67142b8
Grzegorz Ziemski <ziemski@gmail.com>
2025-02-10 15:30:03 +0100

    Iterative Merge Sort

--- src/main/java/org/example/IterativeMergeSort.java
+++ src/main/java/org/example/IterativeMergeSort.java
@@ -13,12 +13,14 @@ public class IterativeMergeSort {
         int[] temp = Arrays.copyOf(array, n);
 
         for (int size = 1; size < n; size *= 2) {
+            int mergedSize = 2 * size;
 
-            for (int left = 0; left < n - size; left += 2 * size) {
+            for (int left = 0; left < n - size; left += mergedSize) {
+
                 int mid = left + size - 1;
-                int right = Math.min(left + 2 * size - 1, n - 1);
-                System.out.printf("[%d,%d][%d,%d]", left, mid, mid, right);
+                int right = Math.min(left + mergedSize - 1, n - 1);
 
+                System.out.printf("[%d,%d][%d,%d] ", left, mid, mid+1, right);
                 mergeSortedHalves(array, temp, left, mid, right);
             }
             System.out.println();
@@ -34,18 +36,40 @@ public class IterativeMergeSort {
             int mid,
             int right
     ) {
-        int i = left, j = mid + 1, k = left;
+        int mergedIndex = left;
+        int leftArrayIndex = left;
+        int rightArrayIndex = mid + 1;
 
         // Merge two sorted halves into the temp array
-        while (i <= mid && j <= right) {
-            temp[k++] = (array[i] <= array[j]) ? array[i++] : array[j++];
+        while (leftArrayIndex <= mid && rightArrayIndex <= right) {
+            int leftValue = array[leftArrayIndex];
+            int rightValue = array[rightArrayIndex];
+
+            if (leftValue < rightValue) {
+                temp[mergedIndex] = leftValue;
+                leftArrayIndex++;
+            } else {
+                temp[mergedIndex] = rightValue;
+                rightArrayIndex++;
+            }
+            mergedIndex++;
         }
 
         // Copy remaining elements from the left half
-        while (i <= mid) temp[k++] = array[i++];
+        while (leftArrayIndex <= mid) {
+            temp[mergedIndex] = array[leftArrayIndex];
+            leftArrayIndex++;
+            mergedIndex++;
+        }
 
+        /*
+        // It won't fail for single‐thread scenario.
         // Copy remaining elements from the right half
-        while (j <= right) temp[k++] = array[j++];
+        while (rightArrayIndex <= right) {
+            temp[mergedIndex] = array[rightArrayIndex];
+            rightArrayIndex++;
+            mergedIndex++;
+        }*/
 
         // Copy sorted elements back to the original array
         for (int x = left; x <= right; x++) {
--- src/test/java/org/example/IterativeMergeSortTest.java
+++ src/test/java/org/example/IterativeMergeSortTest.java
@@ -15,7 +15,8 @@ public class IterativeMergeSortTest {
     @ParameterizedTest
     @MethodSource("provideTestCases")
     public void testIterativeMergeSort(int[] input, int[] expected) {
-        var actual = IterativeMergeSort.sort(input.clone());
+        var actual = input.clone();
+        IterativeMergeSort.sort(actual);
         try {
             assertArrayEquals(expected, actual);
         } catch (AssertionFailedError e) {
@@ -39,11 +40,11 @@ public class IterativeMergeSortTest {
 
     @ParameterizedTest
     @MethodSource("provideMergeSortedHalvesTestCases")
-    public void testMergeSortedHalves(int[] input, int[] expected) {
+    public void testMergeSortedHalves(int[] input, int[] expected, int mid) {
 
         var actual = input.clone();
         var temp = input.clone();
-        IterativeMergeSort.mergeSortedHalves(actual, temp, 0, 1, actual.length - 1);
+        IterativeMergeSort.mergeSortedHalves(actual, temp, 0, mid, actual.length - 1);
 
         try {
             assertArrayEquals(expected, actual);
@@ -57,7 +58,10 @@ public class IterativeMergeSortTest {
 
     private static Stream<Arguments> provideMergeSortedHalvesTestCases() {
         return Stream.of(
-                Arguments.of(new int[]{1,3,2,4}, new int[]{1,2,3,4}) // Sorted
+                //Arguments.of(new int[]{2,1}, new int[]{1,2}, 0),
+                //Arguments.of(new int[]{1,3,2,4}, new int[]{1,2,3,4}, 1),
+                //Arguments.of(new int[]{1,3,5,2,4}, new int[]{1,2,3,4,5}, 2) // Left is longer, copy elements from right at the end.
+                Arguments.of(new int[]{1,2,3,5,6}, new int[]{1,2,3,5,6}, 2) // Right is longer, copy elements from right at the end.
                 //Arguments.of(new int[]{-3, -2, -1, 0, 1, 2, 3}, new int[]{-3, -2, -1, 0, 1, 2, 3}), // Sorted
                 //Arguments.of(new int[]{3, 2, 1, 0, -1, -2, -3}, new int[]{-3, -2, -1, 0, 1, 2, 3}), // Reversed
                 //Arguments.of(new int[]{2, 2, 1, 1, 0, -1, -2, -2}, new int[]{-2, -2, -1, 0, 1, 1, 2, 2}), // Duplicates
